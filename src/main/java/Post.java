44894eaa410c5f56f293ea157e76d0e7b7cf54fc
Demidoff Serg <serega.dem@inbox.ru>
2023-06-10 17:42:55 +0800

    add change

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Post {
+public class Post {
+
+    FormDate birthday = new FormDate();
+
+    public String name;
+    public String passport;
+    public String surname;
+    public String phone;
+    public String patronymic;
+    public boolean subscription;
+
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.name = "Иван";
+        post.passport = "4444 № 444444";
+        post.surname = "Иванов";
+        post.phone = "+7 (999)-999-99-99";
+        post.patronymic = "Иванович";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
 }
+
